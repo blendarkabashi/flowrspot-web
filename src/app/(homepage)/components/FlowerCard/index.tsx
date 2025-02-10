e73fccb26d7da73bbf6f43ffbd7b742e7e0e513e
blendar-opgny <blendar@opgny.com>
2025-02-10 19:26:49 +0100

    add anchor tag to favorite icon

--- src/app/(homepage)/components/FlowerCard/index.tsx
+++ src/app/(homepage)/components/FlowerCard/index.tsx
@@ -19,10 +19,12 @@ const FlowerCard = ({ flower }: { flower: Flower }) => {
     >
       <div className="absolute top-0 left-0 right-0 bottom-0 bg-gradient-to-b from-[rgba(0,0,0,0.0001)] to-[rgba(0,0,0,0.7)] to-[89.5%]"></div>
       <div className="flex flex-col justify-between h-full p-5 relative z-10">
-        <img
-          src={"/images/fav-icon-inactive.svg"}
-          className="w-[30px] ml-auto"
-        ></img>
+        <a href="#">
+          <img
+            src={"/images/fav-icon-inactive.svg"}
+            className="w-[30px] ml-auto"
+          ></img>
+        </a>
         <div className="text-center">
           <div className="text-[20px] text-white">{flower.name}</div>
           <div className="italic text-[12px] opacity-70 text-white mb-[20px]">
